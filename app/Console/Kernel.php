61c28d7dfd45d51941bc3d2b71d8d4cabbc8ce6f
Roman Ganoshenko <roman.coding@gmail.com>
2018-06-02 09:27:05 +0300

    library schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('library:sync-storage')->everyTenMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
