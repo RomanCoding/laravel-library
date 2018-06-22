c6117655e1de31936b571dc1a13d82a5c5bd02f9
Roman Ganoshenko <roman.coding@gmail.com>
2018-06-22 08:51:11 +0300

    webinars test

--- resources/assets/js/components/Webinars.vue
+++ resources/assets/js/components/Webinars.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="row">
-        <div class="col-4">
-            <iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            <p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-        </div>
-        <div class="col-4">
-            <iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            <p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-        </div>
-        <div class="col-4">
-            <iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            <p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-        </div>
+        TEST CONTENT
+        <!--<div class="col-4">-->
+            <!--<iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <!--<p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>-->
+        <!--</div>-->
+        <!--<div class="col-4">-->
+            <!--<iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <!--<p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>-->
+        <!--</div>-->
+        <!--<div class="col-4">-->
+            <!--<iframe src="https://player.vimeo.com/video/275691576" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
+            <!--<p><a href="https://vimeo.com/275691576">A Self-Induced Hallucination (2018)</a> from <a href="https://vimeo.com/user15538306">The Eyeslicer</a> on <a href="https://vimeo.com">Vimeo</a>.</p>-->
+        <!--</div>-->
 
     </div>
 </template>
