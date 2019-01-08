7eaecfca714ba3f125930a3620fd58c614156458
Roman Hanoshenko <roman.coding@gmail.com>
2019-01-08 14:31:48 +0200

    fix

--- resources/assets/js/components/BusinessPartners.vue
+++ resources/assets/js/components/BusinessPartners.vue
@@ -7,124 +7,18 @@
                 To find marketing collateral and referral process notes, please visit the Business Partners section of the <u>toolkit</u>.
             </p>
         </div>
-        <partner img="/images/partners/acacia.png" contact="Greg Kentish" mail="ceo@acaciaconnection.com" phone="0417 023 287"
-        url="www.acaciaconnection.com">
-            <template slot="title">Acacia Connection</template>
-            <template slot="service">Employee Assistance Program</template>
-            <template slot="about">Acacia Connection is one of Australia’s leading Employee Assistance providers.
-                Acacia Connection are different to most EAP providers as they really care about people as individuals, not transactions.
-                They work very closely with clients and provide them with the highest standard of service and support.
-                Acacia Connection have a national network of over 650 psychologists and cover all regional and remote areas.
-            </template>
-            <template slot="benefit">Members who refer work to Acacia Connection receive a 10% rebate on finalised sales.</template>
-        </partner>
-        <partner img="/images/partners/blandslaw.jpg" contact="Andrew Bland" mail="enquiries@blandslaw.com.au" phone="(02) 9805 5600"
-                 url="www.blandslaw.com.au">
-            <template slot="title">BlandsLaw</template>
-            <template slot="service">Industrial Relations and Commercial law firm</template>
-            <template slot="about">BlandsLaw is a specialised and effective solution to assist you and your clients in their Industrial Relations needs.
-                Complimenting and supporting the services you provide as an HR Coach, BlandsLaw provides the specialist advice to support your clients.
-            </template>
-            <template slot="benefit">Members who refer work to BlandsLaw receive a 15% rebate on finalised sales.</template>
-        </partner>
-        <partner img="/images/partners/foundu.png" contact="Matthew Horton" mail="matthew@foundu.com.au" phone="0408 748 521"
-                 url="www.foundu.com.au">
-            <template slot="title">foundU</template>
-            <template slot="service">Employee onboarding, rostering and payroll</template>
-            <template slot="about">foundU is an extremely innovative and nimble, Australian based employee rostering and payroll solution.
-                Their system automates and streamlines the transactional side of onboarding new employees, budget reviewed rostering and single touch payroll.
-                If your clients are spending more than an hour on rostering and paying employees a week, then this system will add value to their business.
-            </template>
-            <template slot="benefit">Members who refer work to foundU receive a 5% rebate on finalised sales.</template>
-        </partner>
-        <partner img="/images/partners/hrstaff.jpg" contact="Sam McCleary" mail="admin@hrcoach.com.au" phone="1300 550 674"
-                 url="www.hrstaffmanager.com.au">
-            <template slot="title">HR Staff Manager</template>
-            <template slot="service">Cloud based HR Information System.</template>
-            <template slot="about">HR Staff Manager is a web-based application that helps your clients manage the employee lifecycle process (with all processes underpinned by HR Coach methodology).
-                HR Staff Manager helps streamline the transactional load of HR and upskill your clients employees to administer the system to allow you to focus on working tactically and strategically with your client.
-            </template>
-            <template slot="benefit">Gold Network Members receive a 40% rebate on finalised sales; Bronze Network Members receive a 10% rebate on finalised sales.</template>
-        </partner>
-        <partner img="/images/partners/hrsuccess.jpg" contact="Greg Mitchell" mail="greg@hrsuccess.com.au" phone="1300 783 211"
-                 url="www.hrsuccess.com.au">
-            <template slot="title">HR Success</template>
-            <template slot="service">Customised or pre-built eLearning solutions.</template>
-            <template slot="about">HR Success has an extensive library of eLearning solutions that have been created with HR Coach clients in mind.
-                Clients also have the option of building their own eLearning solutions to ensure consistency across their business.
-            </template>
-            <template slot="benefit">Receive discounted pricing on pre-built courses. Commission on custom builds by negotiation.</template>
-        </partner>
-        <partner img="/images/partners/integro.jpg" contact="Keith Ayers" mail="support@integro.com.au" phone="1800 222 902"
-                 url="www.integro.com.au">
-            <template slot="title">Integro</template>
-            <template slot="service">Personality profiling, effective team building and conflict resolution tools.</template>
-            <template slot="about">Intégro are worldwide leading distributors of Everything DiSC® and The Five Behaviors of a Cohesive Team™
-                who have been changing Australian workplaces with knowledge and experience in these solutions for over 30 years.
-            </template>
-            <template slot="benefit">Receive discounted wholesale EPIC credit pricing and Everything DiSC accreditations.</template>
-        </partner>
-        <partner img="/images/partners/mybusiness.jpg" contact="Sam McCleary" mail="admin@hrcoach.com.au" phone="1300 550 674"
-                 url="www.mybhc.com.au">
-            <template slot="title">My Business Health Check</template>
-            <template slot="service">Basic business diagnostic tool</template>
-            <template slot="about">My Business Health Check is a proprietary online tool of HR Coach Australasia.
-                The Business health Check can be used to open the door to new or existing clients by asking them to complete a survey on their business, not just HR.
-                Clients then receive a one page summary and the Coach receives a full report, complete with recommendations to toolkit and Business Partners solutions.
-            </template>
-            <template slot="benefit">Gold Network Members receive a 50% discount on processing fees.</template>
-        </partner>
-        <partner img="/images/partners/omnia.png" contact="Kate Brown" mail="admin@omniaprofiling.com.au" phone="0411 164 431"
-                 url="www.omniaprofiling.com.au">
-            <template slot="title">Omnia</template>
-            <template slot="service">Cognitive assessments and selection profiling</template>
-            <template slot="about">Omnia provide selection profiling and cognitive assessments to support hiring decisions.
-                Take the guess work out of recruitment and rely on data to make the right choices for your clients.
-            </template>
-            <template slot="benefit">Receive discounted wholesale pricing for all products</template>
-        </partner>
-        <partner img="/images/partners/career.png" contact="Sam McCleary" mail="admin@hrcoach.com.au" phone="1300 550 674"
-                 url="www.careermonitor.com.au">
-            <template slot="title">Online Career Monitor</template>
-            <template slot="service">Key Employee Retention Interviews</template>
-            <template slot="about">Online interview tool to understand the motivators for key employees in a business with direct comparison between why they joined and why they stay.
-                Use this tool to recognize key employees and ensure your client is doing everything they can to stop them moving on.
-            </template>
-            <template slot="benefit">Proprietary tool of HR Coach Australasia with no competitors in the market.
-                Gold Network Members receive 2 free Online Career Monitor’s each year.
-            </template>
-        </partner>
-        <partner img="/images/partners/onlinewhs.png" contact="Phil Bamford" mail="phil@onlinecompliance.com.au" phone="1800 020 389"
-                 url="www.onlinecompliancesystems.com.au">
-            <template slot="title">Online Compliance Systems</template>
-            <template slot="service">Cloud based safety management system</template>
-            <template slot="about">Online Compliance Systems (OCS) has been helping organisations manage their WHS requirements and create safer work environments for over eight years.
-                OCS’s flagship product is Online WHS - a unique cloud-based system, which enables organisations to centralise
-                and automate much of their WHS regulatory and operational processes, saving them time and money.
-            </template>
-            <template slot="benefit">Members who refer work to Online Compliance receive between 7.5% and 12.5% (depending on level of involvement) rebate on finalised sales.</template>
-        </partner>
-        <partner img="/images/partners/starworkplace.jpg" contact="Sam McCleary" mail="admin@hrcoach.com.au" phone="1300 550 674"
-                 url="www.starworkplace.com.au">
-            <template slot="title">STAR Workplace Program</template>
-            <template slot="service">Employer and employee engagement and alignment tool</template>
-            <template slot="about">STAR Workplace Program is the only engagement and alignment tool on the market that recognises the impact employers have on strategic
-                alignment. Backed by over 10 years of research, results are benchmarked against over 700 Australasian businesses.
-            </template>
-            <template slot="benefit">Proprietary tool of HR Coach Australasia, Gold Network Members receive 75% discount on processing fees;
-                Bronze Network Members receive 50% discount on processing fees.
-            </template>
-        </partner>
-        <partner img="/images/partners/templetons.png" contact="Eric Chong" mail="e.chong@templetons.com.au" phone="0488 488 171"
-                 url="www.amp.com.au/ampadvice/templetons" :divider="false">
-            <template slot="title">Templetons Financial</template>
-            <template slot="service">Individual financial planning services and corporate superannuation specialists</template>
-            <template slot="about">Templetons Financial is a member of AMP Advice, providing coverage across Australia for HR Coaches and their clients. Templetons provide a cost effective solution
-                for corporate superannuation funds as well as individual financial advice and outplacement sessions to help understand the financial implications of redundancy / termination.
-            </template>
-            <template slot="benefit">Members who refer work to Online Compliance receive a 15% rebate on finalised sales.
-                Network Members also receive discounts on their own individual financial planning.
-            </template>
+
+        <partner v-for="(partner, index) in partners"
+                 :img="partner.img"
+                 :contact="partner.contact"
+                 :mail="partner.email"
+                 :phone="partner.phone"
+                 :url="partner.url"
+                 :divider="(index !== partners.length - 1)">
+            <template slot="title" v-text="partner.title"></template>
+            <template slot="service" v-text="partner.service"></template>
+            <template slot="about" v-text="partner.about"></template>
+            <template slot="benefit" v-text="partner.benefit"></template>
         </partner>
         <footer class="px-4 m-4">
             © HR Coach Australasia 2018. This website and content is the intellectual property of HR Coach Australasia. Any disclosure, copying, display or misuse of this information is strictly prohibited without the written consent of HR Coach Australasia.
@@ -137,7 +31,17 @@
     export default {
         components: {
             'partner': Partner,
-        }
+        },
+        data() {
+            return {
+                partners: [],
+            }
+        },
+        created() {
+            axios.get('/partners').then(response => {
+                this.partners = response.data;
+            });
+        },
     }
 </script>
 
